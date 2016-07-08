2aa32a54b6d2b6b36d491e4b4431bff4eb087fe0
Jesus Angulo <jesus.angulo@outlook.com>
2016-07-08 00:06:33 -0500

    Visual Modificactions

--- LearnIT/Models/Photos/UploadViewModel.cs
+++ LearnIT/Models/Photos/UploadViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace LearnIT.Models.Photos
     public class UploadViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Tags { get; set; }
         public string UserId { get; set; }
         public string Image { get; set; }
